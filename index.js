56d5a469dbfe4a4aa5c42d402a0f1d7bd421489b
SalamanderCtesiphon <swbrookshire@gmail.com>
2025-02-26 16:28:37 -0600

    created a function that iterates through an array and creates a diplay card for each individual object also added a button to use to add a new book

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function Book(title, author, pages, haveRead) {
   this.pages = pages;
   this.haveRead = haveRead;
   this.info = function () {
-    return `${this.title} by ${this.author}, ${this.pages}, ${haveRead}`;
+    return `${this.title} by ${this.author}, ${this.pages}, ${this.haveRead}`;
   };
 };
 
@@ -16,5 +16,34 @@ function addBookToLibrary(title, author, pages, haveRead) {
   return myLibrary;
 };
 addBookToLibrary("The Hobbit", "J.R.R. Tolkien", "295 pages", "have read");
+addBookToLibrary("rando", "rando", "rando", "rando");
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", "295 pages", "have read");
+addBookToLibrary("rando", "rando", "rando", "rando");
 
-console.log(myLibrary);
+const bookDisplay = document.querySelector("#bookDisplay");
+function displayBooks(bookDisplay) {
+  for(const obj of myLibrary) {
+    const bookCard = document.createElement("div");
+    const bookTitle = document.createElement("h3");
+    const bookAuthor = document.createElement("p");
+    const bookPages = document.createElement("p");
+    const bookHaveRead = document.createElement("p");
+    bookCard.classList.add("card");
+    bookTitle.textContent = obj.title;
+    bookAuthor.textContent = obj.author;
+    bookPages.textContent = obj.pages;
+    bookHaveRead.textContent = obj.haveRead;
+    bookCard.appendChild(bookTitle);
+    bookCard.appendChild(bookAuthor);
+    bookCard.appendChild(bookPages);
+    bookCard.appendChild(bookHaveRead);
+    bookDisplay.appendChild(bookCard);
+  };
+};
+
+displayBooks(bookDisplay);
+
+myLibrary.forEach((value) => {
+    
+    
+  });
